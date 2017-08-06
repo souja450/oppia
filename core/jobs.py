souja450/oppia

commit 7cd525a088f880bc29f0f25f747e6c3e04aa406b

Introduce output list formatting method to core.jobs.BaseJobManager (#3671)
* Adds some tests for the new _compress_output_list method

* Moves output list compression to its own function.

* _compress_output_list now passes all tests.

* Temporarily disable relative-import

* Allow protected access to method being tested.

* Re-enable relative-import

* Better naming for tests

* Integrates review suggestions

* Improve phrasing.

* Improve naming and documentation of the test-only override setting.

* Minor phrasing improvements.

* Adds testcase for exactly matching max_output_len.

* Typo: %s/intendend/intended

* Reordered tests since some tests rely on previous ones passing.

core/jobs.py
@@ -192,35 +192,52 @@ class BaseJobManager(object):
 
         model.status_code = STATUS_CODE_COMPLETED
         model.time_finished_msec = utils.get_current_time_in_millisecs()
-
-        # TODO(bhenning): Add tests for this.
-        output_str_list = ['%s' % output_value for output_value in output_list]
-
-        # De-duplicate the lines of output since it's not very useful to repeat
-        # them.
-        counter = collections.Counter(list(output_str_list))
-        output_str_frequency_list = [
-            (output_str, counter[output_str]) for output_str in counter]
-        output_str_list = [
-            line if freq == 1 else '%s (%d times)' % (line, freq)
-            for (line, freq) in output_str_frequency_list
-        ]
-
-        cutoff_index = 0
-        total_output_size = 0
-        for idx, output_str in enumerate(output_str_list):
-            cutoff_index += 1
-            total_output_size += len(output_str)
-            if total_output_size >= _MAX_OUTPUT_LENGTH_CHARS:
-                max_element_length = (
-                    total_output_size - _MAX_OUTPUT_LENGTH_CHARS)
-                output_str_list[idx] = output_str[:max_element_length]
-                output_str_list[idx] += ' <TRUNCATED>'
-                break
-        model.output = output_str_list[:cutoff_index]
+        model.output = cls._compress_output_list(output_list)
         model.put()
 
         cls._post_completed_hook(job_id)
+
+    @classmethod
+    def _compress_output_list(cls, output_list, test_only_max_output_len=None):
+        """Returns compressed list of strings within a max length of chars.
+
+        Ensures that the payload (i.e., [str(output) for output in output_list])
+        makes up at most max_output_chars of the final output data.
+
+        Args:
+            output_list: list(*). Collection of objects to be stringified.
+            test_only_max_output_len: int or None. Overrides the intended max
+                output len limit when not None.
+
+        Returns:
+            list(str). The compressed stringified output values.
+        """
+        _MAX_OUTPUT_LEN = 900000
+
+        # Consolidate the lines of output since repeating them isn't useful.
+        counter = collections.Counter(str(output) for output in output_list)
+        output_str_list = [
+            output_str if count == 1 else '(%dx) %s' % (count, output_str)
+            for (output_str, count) in counter.iteritems()
+        ]
+
+        # Truncate outputs to fit within given max length.
+        if test_only_max_output_len is None:
+            remaining_len = _MAX_OUTPUT_LEN
+        else:
+            remaining_len = test_only_max_output_len
+
+        for idx, output_str in enumerate(output_str_list):
+            remaining_len -= len(output_str)
+            if remaining_len < 0:
+                # Truncate all remaining output to fit in the limit.
+                kept_str = output_str[:remaining_len]
+                output_str_list[idx:] = [
+                    ('%s <TRUNCATED>' % kept_str) if kept_str else '<TRUNCATED>'
+                ]
+                break
+
+        return output_str_list
 
     @classmethod
     def register_failure(cls, job_id, error):

core/jobs_test.py
@@ -362,6 +362,56 @@ class JobManagerUnitTests(test_utils.GenericTestBase):
             'Canceled by admin_user_id', DummyJobManager.get_error(job1_id))
         self.assertIsNone(DummyJobManager.get_error(job2_id))
 
+    def test_compress_output_list_with_single_char_outputs(self):
+        sample_output_list = [1, 2, 3, 4, 5]
+        expected = ['1', '2', '3', '<TRUNCATED>']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list, test_only_max_output_len=3)
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_multi_char_outputs(self):
+        sample_output_list = ['abcd', 'efgh', 'ijkl']
+        expected = ['abcd', 'efgh', 'ij <TRUNCATED>']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list, test_only_max_output_len=10)
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_zero_max_output_len(self):
+        sample_output_list = [1, 2, 3]
+        expected = ['<TRUNCATED>']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list, test_only_max_output_len=0)
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_exact_max_output_len(self):
+        sample_output_list = ['abc']
+        expected = ['abc']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list, test_only_max_output_len=3)
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_empty_outputs(self):
+        sample_output_list = []
+        expected = []
+        actual = jobs.BaseJobManager._compress_output_list(sample_output_list)  # pylint: disable=protected-access
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_duplicate_outputs(self):
+        sample_output_list = ['bar', 'foo'] * 3
+        expected = ['(3x) bar', '(3x) foo']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list,
+            # Ensure truncation doesn't happen in this test.
+            test_only_max_output_len=sum(len(s) for s in expected))
+        self.assertEquals(expected, sorted(actual))
+
+    def test_compress_output_list_with_truncated_duplicate_outputs(self):
+        sample_output_list = ['supercalifragilisticexpialidocious'] * 3
+        expected = ['(3x) super <TRUNCATED>']
+        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            sample_output_list, test_only_max_output_len=10)
+        self.assertEquals(expected, sorted(actual))
+
 
 SUM_MODEL_ID = 'all_data_id'
 

commit 597d7f17ea1f8535975a320c653034aebac1d33b

Improvements and bug fixes to changes introduced by PR #3671 (#3684)
* Merged to upstream/develop

* Remove mysterious help directory

* Removed unused variable

* Improvements in code-readability

* Make compress_output_tests comprehensive of possibilities.

* Removes unused itertools import

* sample_output_list looks better :)

* Use sets to improve readability of expected_possibilities.

* Use sets for testing multi-elements outputs for consistency

* Fix accidental use of sets where list was required.

* Just use sets everywhere :)

* Improved consistency of testing variable names.

* More renaming fun.

* Applies fixes suggested in PR review.

* Removes manual-testing code changes.

* Removes unused imports

* Improves placement of comments and class definition.

* Rearranges actual and expected.

* Minor formatting improvements to core.jobs

* Revert core.domain.user_jobs_one_off to develop.

* Fixes core.domain.user_jobs_one_off

* Reorders expected elements to reflect what the new output will be (in sorted order).

core/jobs.py
@@ -182,8 +182,6 @@ class BaseJobManager(object):
             job_id: str. The ID of the job to complete.
             output_list: list(object). The output produced by the job.
         """
-        _MAX_OUTPUT_LENGTH_CHARS = 900000
-
         # Ensure that preconditions are met.
         model = job_models.JobModel.get(job_id, strict=True)
         cls._require_valid_transition(
@@ -198,7 +196,8 @@ class BaseJobManager(object):
         cls._post_completed_hook(job_id)
 
     @classmethod
-    def _compress_output_list(cls, output_list, test_only_max_output_len=None):
+    def _compress_output_list(
+            cls, output_list, test_only_max_output_len_chars=None):
         """Returns compressed list of strings within a max length of chars.
 
         Ensures that the payload (i.e., [str(output) for output in output_list])
@@ -206,27 +205,34 @@ class BaseJobManager(object):
 
         Args:
             output_list: list(*). Collection of objects to be stringified.
-            test_only_max_output_len: int or None. Overrides the intended max
-                output len limit when not None.
+            test_only_max_output_len_chars: int or None. Overrides the intended
+                max output len limit when not None.
 
         Returns:
             list(str). The compressed stringified output values.
         """
-        _MAX_OUTPUT_LEN = 900000
+        _MAX_OUTPUT_LEN_CHARS = 900000
+
+        class _OrderedCounter(collections.Counter, collections.OrderedDict):
+            """Counter that remembers the order elements are first encountered.
+
+            We use this class so that our tests can rely on deterministic
+            ordering, instead of simply using `collections.Counter` which has
+            non-deterministic ordering.
+            """
+            pass
 
         # Consolidate the lines of output since repeating them isn't useful.
-        counter = collections.Counter(str(output) for output in output_list)
+        counter = _OrderedCounter(str(output) for output in output_list)
         output_str_list = [
             output_str if count == 1 else '(%dx) %s' % (count, output_str)
             for (output_str, count) in counter.iteritems()
         ]
 
         # Truncate outputs to fit within given max length.
-        if test_only_max_output_len is None:
-            remaining_len = _MAX_OUTPUT_LEN
-        else:
-            remaining_len = test_only_max_output_len
-
+        remaining_len = (
+            _MAX_OUTPUT_LEN_CHARS if test_only_max_output_len_chars is None else
+            test_only_max_output_len_chars)
         for idx, output_str in enumerate(output_str_list):
             remaining_len -= len(output_str)
             if remaining_len < 0:

core/domain/user_jobs_one_off_test.py
@@ -305,7 +305,7 @@ class LongUserBiosOneOffJobTests(test_utils.GenericTestBase):
         user_id_c = self.get_user_id_from_email(self.USER_C_EMAIL)
         user_services.update_user_bio(user_id_c, self.USER_C_BIO)
         result = self._run_one_off_job()
-        expected_result = [[2400, ['d']], [800, ['c']]]
+        expected_result = [[800, ['c']], [2400, ['d']]]
         self.assertEqual(result, expected_result)
 
 

core/jobs_test.py
@@ -363,54 +363,54 @@ class JobManagerUnitTests(test_utils.GenericTestBase):
         self.assertIsNone(DummyJobManager.get_error(job2_id))
 
     def test_compress_output_list_with_single_char_outputs(self):
-        sample_output_list = [1, 2, 3, 4, 5]
-        expected = ['1', '2', '3', '<TRUNCATED>']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=3)
-        self.assertEquals(expected, sorted(actual))
+        input_list = [1, 2, 3, 4, 5]
+        expected_output = ['1', '2', '3', '<TRUNCATED>']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list, test_only_max_output_len_chars=3)
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_multi_char_outputs(self):
-        sample_output_list = ['abcd', 'efgh', 'ijkl']
-        expected = ['abcd', 'efgh', 'ij <TRUNCATED>']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=10)
-        self.assertEquals(expected, sorted(actual))
+        input_list = ['abcd', 'efgh', 'ijkl']
+        expected_output = ['abcd', 'efgh', 'ij <TRUNCATED>']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list, test_only_max_output_len_chars=10)
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_zero_max_output_len(self):
-        sample_output_list = [1, 2, 3]
-        expected = ['<TRUNCATED>']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=0)
-        self.assertEquals(expected, sorted(actual))
+        input_list = [1, 2, 3]
+        expected_output = ['<TRUNCATED>']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list, test_only_max_output_len_chars=0)
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_exact_max_output_len(self):
-        sample_output_list = ['abc']
-        expected = ['abc']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=3)
-        self.assertEquals(expected, sorted(actual))
+        input_list = ['abc']
+        expected_output = ['abc']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list, test_only_max_output_len_chars=3)
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_empty_outputs(self):
-        sample_output_list = []
-        expected = []
-        actual = jobs.BaseJobManager._compress_output_list(sample_output_list)  # pylint: disable=protected-access
-        self.assertEquals(expected, sorted(actual))
+        input_list = []
+        expected_output = []
+        actual_output = jobs.BaseJobManager._compress_output_list(input_list)  # pylint: disable=protected-access
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_duplicate_outputs(self):
-        sample_output_list = ['bar', 'foo'] * 3
-        expected = ['(3x) bar', '(3x) foo']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list,
-            # Ensure truncation doesn't happen in this test.
-            test_only_max_output_len=sum(len(s) for s in expected))
-        self.assertEquals(expected, sorted(actual))
+        input_list = ['bar', 'foo'] * 3
+        expected_output = ['(3x) bar', '(3x) foo']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list,
+            # Make sure no output gets truncated.
+            test_only_max_output_len_chars=sum(len(s) for s in expected_output))
+        self.assertEquals(actual_output, expected_output)
 
     def test_compress_output_list_with_truncated_duplicate_outputs(self):
-        sample_output_list = ['supercalifragilisticexpialidocious'] * 3
-        expected = ['(3x) super <TRUNCATED>']
-        actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=10)
-        self.assertEquals(expected, sorted(actual))
+        input_list = ['supercalifragilisticexpialidocious'] * 3
+        expected_output = ['(3x) super <TRUNCATED>']
+        actual_output = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
+            input_list, test_only_max_output_len_chars=10)
+        self.assertEquals(actual_output, expected_output)
 
 
 SUM_MODEL_ID = 'all_data_id'